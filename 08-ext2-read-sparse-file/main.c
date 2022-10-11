6330645d0ffef6b0e6fbff30aaf96c4cea3539ba
Dominique Lefevre <root@/dev/null>
2022-10-11 06:10:46 -0400

    exercises: add 08-ext2-read-sparse-file.

---
+++ 08-ext2-read-sparse-file/main.c
@@ -0,0 +1,29 @@
+#include <solution.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <err.h>
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	int img = open("img", O_RDONLY);
+	int out = open("out", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+
+	if (img < 0)
+		errx(1, "open(img) failed");
+	if (out < 0)
+		errx(1, "open(out) failed");
+
+	/* 2 is the inode nr. of the root directory */
+	int r = dump_file(img, 2, out);
+	if (r < 0)
+		errx(1, "dump_file() failed");
+
+	close(out);
+	close(img);
+
+	return 0;
+}
