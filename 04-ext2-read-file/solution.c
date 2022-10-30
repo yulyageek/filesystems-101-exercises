b2087ec8aab2500b1abe96c27b9d40b4bbfa105d
yulyaageek <bodrova.ium@phystech.edu>
2022-10-30 16:41:05 +0300

    copy: single-indirect read

--- 04-ext2-read-file/solution.c
+++ 04-ext2-read-file/solution.c
@@ -1,11 +1,56 @@
 #include <solution.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 
 int dump_file(int img, int inode_nr, int out)
 {
 	(void) img;
 	(void) inode_nr;
 	(void) out;
+	
+	struct ext2_super_block sb;
+	lseek(img, 1024, SEEK_SET);
+	int len  = read(img, &sb, sizeof(struct ext2_super_block));
+	if(len < 0){
+		return -errno;
+	}
+	__u32 block_size = 1024 << sb.s_log_block_size;
 
-	/* implement me */
+	struct ext2_group_desc gd;
+	lseek(img, block_size * (sb.s_first_data_block + 1), SEEK_SET);
+	len  = read(img, &gd, sizeof(struct ext2_group_desc));
+	if(len < 0){
+		return -errno;
+	}
+	
+	lseek(img, block_size * (gd.bg_inode_table) + (inode_nr - 1) * sizeof(struct ext2_inode), SEEK_SET);
+	struct ext2_inode in;
+	len  = read(img, &in, sizeof(struct ext2_inode));
+	if(len < 0){
+		return -errno;
+	}
+
+	char *buf = (char*)malloc(block_size);
+	for(int i = 0; i < 15; i++){
+		if(!in.i_block[i]){
+			break;
+		}
+		lseek(img, block_size * in.i_block[i], SEEK_SET);
+		len = read(img, buf, block_size);
+		if(len < (int)block_size){
+			free(buf);
+			return -errno;
+		}
+		len = write(out, buf, block_size);
+		if(len < (int)block_size){
+			free(buf);
+			return -errno;
+		}
+		free(buf);
+	}
+
 	return 0;
 }
