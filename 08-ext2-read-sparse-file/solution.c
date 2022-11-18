01f9e855fcc50b6cc5ddd683c5c5007c0cb18501
yulyaageek <bodrova.ium@phystech.edu>
2022-11-18 03:49:45 +0300

    check size

--- 08-ext2-read-sparse-file/solution.c
+++ 08-ext2-read-sparse-file/solution.c
@@ -130,6 +130,6 @@ int dump_file(int img, int inode_nr, int out)
 	if(ret < 0){
 		return ret;
 	}
-	//fprintf(stderr, "my_size: %d, file_size: %d", copy_offset, size);
-	return 0;
+	fprintf(stderr, "my_size: %d, file_size: %d", copy_offset, size);
+	return -1;
 }
